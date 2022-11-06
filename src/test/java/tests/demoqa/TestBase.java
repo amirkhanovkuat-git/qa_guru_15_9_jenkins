c6d179bab53c428fc2a925951ee34b713f4e39cb
Kuat <amirkhanovkuat@gmail.com>
2022-11-07 00:46:40 +0600

    added remote configuration

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -9,6 +9,6 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1280x800";
-
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
